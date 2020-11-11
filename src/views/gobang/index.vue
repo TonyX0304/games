5d909dbe888c25faac74ea0a3bde54097d55ae8a
肖甜 <tony-x@yungehuo.com>
2020-11-11 09:38:22 +0800

    Revert "Revert "五子棋""
    This reverts commit b10df0e1d8c1027d8b2ec7a7d4f73cbce82fc34d.

--- src/views/gobang/index.vue
+++ src/views/gobang/index.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <div class="game-box">
-      <ul v-for="(ul, uIndex) in list" :key="uIndex" :data-row="uIndex+1">
-        <li v-for="(li, lIndex) in ul"  :key="lIndex"  :data-col="lIndex+1"> 
-          <div class="piece" :class="[curPlayer === 'p1' ? 'whiting' : 'blacking', {white: li.value === 'p1'}, {black: li.value === 'p2'} ]" @click="playing(li)" />
-          <!-- {{ lIndex + 1 }} -->
-        </li>
-      </ul>
-    </div>
+  <div class="game-box">
+    <button @click="reset">重新开始</button>
+    <ul class="player-list">
+      <h2>
+        {{ winner ? '获胜方为：' : '落子方：' }}
+      </h2>
+      <li v-if="winner" :class="winner === 'p1' ? 'white' : 'black'" />
+      <template v-else>
+        <li class="whiting" v-show="curPlayer === 'p1'"/>
+        <li class="black" v-show="curPlayer === 'p2'" />
+      </template>
+    </ul>
+    <ul v-for="(ul, uIndex) in list" :key="uIndex" :data-row="uIndex+1" class="list-box">
+      <li v-for="(li, lIndex) in ul"  :key="lIndex"  :data-col="lIndex+1"> 
+        <div 
+          class="piece" 
+          :class="[curPlayer === 'p1' ? 'whiting' : 'blacking', 
+            {'white': li.value === 'p1'}, 
+            {'black': li.value === 'p2'}, 
+            {'active': li.success}, 
+            {'disabled': winner} 
+          ]" 
+          @click="playing(li)" 
+        />
+        <!-- {{ uIndex+','+lIndex }} -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,7 +37,8 @@ export default {
       list: [],
       xLength: 15,
       yLength: 15,
-      curPlayer: 'p1'
+      curPlayer: 'p2',
+      winner: null
     }
   },
   mounted() {
@@ -27,12 +46,13 @@ export default {
   },
   methods: {
     init() {
-      for(let i=0; i< this.xLength; i++) {
+      this.list = []
+      for(let i=0; i< this.yLength; i++) {
         const item = []
-        for(let j=0; j< this.yLength; j++) {
+        for(let j=0; j< this.xLength; j++) {
           item.push({
-            x: i+1,
-            y: j+1,
+            x: j,
+            y: i,
             value: null
           })
         }
@@ -40,20 +60,89 @@ export default {
       }
     },
     playing(item) {
-      if (item.value) {
+      if (item.value || this.winner) {
         return
       } else {
         item.value = this.curPlayer
-        this.isOver(item)
+        this.check(item)
         this.curPlayer = this.curPlayer === 'p1' ? 'p2' : 'p1'
       }
     },
-    isOver(item) {
-      console.log(111)
-      // 横向试探
-      const xmin = item.x - 4
+    check(item) {
+      const xMin = item.x - 4
       const xMax = item.x + 4
-      const leftStep = 4 - 0
+      const yMin = item.y - 4
+      const xList = [] // 横向
+      const yList = [] // 纵向
+      const rotatePList = [] // 正向倾斜
+      const rotateMList = [] // 负向倾斜
+      for (let i=0; i < 9; i++) {
+        const curXMin = xMin + i, curYMin = yMin + i
+        const curXMax = xMax - i
+        if(curXMin >= 0 && curXMin <= this.xLength -1) {
+          xList.push({x: curXMin, y: item.y}) // 横向
+          if(curYMin >= 0 && curYMin <= this.yLength -1) {
+            rotateMList.push({x: curXMin, y: curYMin}) //  负向倾斜
+          }
+        }
+
+        if(curYMin >= 0 && curYMin <= this.yLength -1) {
+          yList.push({x: item.x, y: curYMin}) // 纵向
+          if(curXMax >= 0 && curXMax <= this.xLength -1) {
+            rotatePList.push({x: curXMax, y: curYMin}) // 正向倾斜
+          }
+        }
+      }
+      // console.log(xList, yList, rotatePList, rotateMList)
+      const testXlist = this.isOver(xList, item)
+      if(testXlist) {
+        return
+      }
+      const testYlist = this.isOver(yList, item)
+      if(testYlist) {
+        return
+      }
+      const testRPlist = this.isOver(rotatePList, item)
+      if(testRPlist) {
+        return
+      }
+      this.isOver(rotateMList, item)
+      // console.log(testXlist, testYlist, testRPlist, testRMlist)
+    },
+    /**
+     * 查看数组中是否有5个连续的值与当前元素值一致
+     */
+    isOver(list, item) {
+      if(list.length < 5) {
+        return
+      }
+      let num = 0
+      let lastIndex = null // 记录最后一个满足条件的索引
+      for (let i=0; i < list.length; i++) {
+        const element = list[i]
+        if(this.list[element.y][element.x].value === item.value) {
+          num++
+          if(num === 5) {
+            lastIndex = i
+            break
+          }
+        } else {
+          num = 0
+        }
+      }
+      // 标记处满足条件的选项
+      if(lastIndex && num >= 5) {
+        list.forEach((element, index) => {
+          if(index >= lastIndex - 4 && index <= lastIndex ) {
+            this.list[element.y][element.x].success = true
+          }
+        })
+        this.winner = item.value
+      }
+      return num >= 5
+    },
+    reset() {
+      this.init()
     }
   }
 }
@@ -62,10 +151,26 @@ export default {
 <style lang="stylus" scoped>
   .game-box
     margin 20px auto
-    width 800px
     background-color #d6b594
-    padding 50px 20px
+    padding 20px 20px 50px
     ul
+      list-style none
+      padding 0
+      margin 0
+    ul.player-list
+      display flex
+      justify-content center
+      margin-bottom 10px
+      align-items center
+      li
+        width 26px
+        height 26px
+        border-radius 50%
+        list-style none 
+        background-color #fff
+        &.black
+          background-color #000
+    ul.list-box
       list-style none
       padding 0
       margin 0
@@ -79,8 +184,8 @@ export default {
       li
         font-size 14px
         display inline-flex
-        width 50px
-        height 50px
+        width 40px
+        height 40px
         position relative
         border-left 1px solid #aaa
         border-top 1px solid #aaa
@@ -101,10 +206,10 @@ export default {
           left 0
           transform translate(-50%, -50%)
           cursor pointer
-          &.whiting:not(.white):not(.black)
+          &.whiting:not(.white):not(.black):not(.disabled)
             &:hover
               background-color rgba(255,255,255,0.6)
-          &.blacking:not(.white):not(.black)
+          &.blacking:not(.white):not(.black):not(.disabled)
             &:hover
               background-color rgba(0,0,0,0.6)
           &.white
@@ -113,4 +218,6 @@ export default {
           &.black
             background-color #000
             cursor not-allowed
+          &.active
+            border 3px solid green
 </style>
